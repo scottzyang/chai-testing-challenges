91905fdcb327c0f3bc4f419de5ce2a679d1292e5
scottzyang <scottzyang@gmail.com>
2023-05-09 20:46:26 -0500

    Complete messages routes

--- src/routes/message.js
+++ src/routes/message.js
@@ -5,16 +5,27 @@ const User = require('../models/user')
 const Message = require('../models/message')
 
 /** Route to get all messages. */
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     // TODO: Get all Message objects using `.find()`
+    try {
+        const messages = await Messages.find();
+        return res.json({ messages })
+    } catch (err) {
+        return res.json({ message: err.message });
+    }
 
-    // TODO: Return the Message objects as a JSON list
 })
 
 /** Route to get one message by id. */
-router.get('/:messageId', (req, res) => {
+router.get('/:messageId', async (req, res) => {
     // TODO: Get the Message object with id matching `req.params.id`
     // using `findOne`
+    try {
+        const message = await Message.findById(req.params.messageId);
+        return res.json({ message });
+    } catch (err) {
+        return res.json({ message: err.message });
+    }
 
     // TODO: Return the matching Message object as JSON
 })
@@ -39,16 +50,28 @@ router.post('/', (req, res) => {
 })
 
 /** Route to update an existing message. */
-router.put('/:messageId', (req, res) => {
+router.put('/:messageId', async (req, res) => {
     // TODO: Update the matching message using `findByIdAndUpdate`
-
+    try {
+        const message = await Message.findByIdAndUpdate(req.params.messageId, req.body);
+        const updatedMessage = await Message.findById(req.params.messageId);
+        return res.json({ message: updatedMessage });
+    } catch (err) {
+        return res.json({ message: err.message });
+    }
     // TODO: Return the updated Message object as JSON
 })
 
 /** Route to delete a message. */
-router.delete('/:messageId', (req, res) => {
+router.delete('/:messageId', async (req, res) => {
     // TODO: Delete the specified Message using `findByIdAndDelete`. Make sure
     // to also delete the message from the User object's `messages` array
+    try {
+        const message = await Message.findByIdAndDelete(req.params.messageId);
+        return res.json({ message: "Successfully removed" });
+    } catch (err) {
+        res.json({message: err.message});
+    }
 
     // TODO: Return a JSON object indicating that the Message has been deleted
 })
